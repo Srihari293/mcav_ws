1fe81cd2e6aa2b049a5008c5153cd200e3a75710
srihari <ssrihari2002@gmail.com>
2023-01-23 15:10:32 +1100

    Task 1 completed

---
+++ src/ros2_tutorial/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'ros2_tutorial'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='srihari',
+    maintainer_email='srihari2002@gmail.com',
+    description='Control node',
+    license='License',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'control_node = ros2_tutorial.control_node:main',
+        ],
+    },
+)
